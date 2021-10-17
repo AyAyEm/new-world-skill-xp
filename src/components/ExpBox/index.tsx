5aa0988f5ee0cbbc18d256d0140f6472458785ab
AyAyEm <andre20114meira@gmail.com>
2021-10-16 22:12:55 -0300

    Fix: Blank screen when deslecting a skill

--- src/components/ExpBox/index.tsx
+++ src/components/ExpBox/index.tsx
@@ -1,6 +1,8 @@
 import Typography from '@mui/material/Typography';
 import { getXpDifference } from '../../lib';
 
+import { inRange } from '../../lib/utils';
+
 import type { Skill } from '../../types/skills';
 
 interface ExpBoxProps {
@@ -12,10 +14,14 @@ interface ExpBoxProps {
 export function ExpBox(props: ExpBoxProps) {
   const { range, skill } = props;
 
-  const xpDifference = getXpDifference(range[0], range[1], skill).toLocaleString();
+  let xpDifference = 0;
+  if (skill && range && range.length === 2 && inRange(range[0], 0, 200) && inRange(range[1], 0, 200)) {
+    xpDifference = getXpDifference(range[0], range[1], skill);
+  }
+
   return (
     <Typography className={props.className ? props.className : ''}>
-      {`${xpDifference}xp`}
+      {`${xpDifference.toLocaleString()}xp`}
     </Typography>
   )
 }
