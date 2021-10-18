2f78d0ac64665e1b294ebe14e39f8761f5625a48
AyAyEm <andre20114meira@gmail.com>
2021-10-18 04:10:21 -0300

    Feat: added apSumOfNTerms

--- src/lib/utils/math.ts
+++ src/lib/utils/math.ts
@@ -2,6 +2,12 @@
  * Arithmetic progression.
  * @see https://en.wikipedia.org/wiki/Arithmetic_progression
 */
-export const ap = (start: number, n: number, difference: number): number => start + (n - 1) * difference;
+export const ap = (start: number, n: number, diff: number): number => start + (n - 1) * diff;
+
+/**
+ * Sum of n terms for an arithmetic progression.
+ * @see https://byjus.com/maths/sum-of-n-terms
+ */
+export const apSumOfNTerms = (start: number, n: number, diff: number) => n / 2 * (2 * start + (n - 1) * diff);
 
 export default ap;
