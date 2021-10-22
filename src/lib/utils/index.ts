4a38d08ee6994d48ef0ac0eb628f06bd2a15a162
AyAyEm <andre20114meira@gmail.com>
2021-10-22 11:51:06 -0300

    Feat: upperFirst utility

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export * from './math';
 export * from './toSkillRangeXp';
 export * from './inRange';
 export * from './skills';
+export * from './upperFirst';
---
+++ src/lib/utils/upperFirst.ts
@@ -0,0 +1,1 @@
+export const upperFirst = (s: string) => s[0].toUpperCase() + s.slice(1);
