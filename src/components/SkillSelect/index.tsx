f13ef6a040ce67bfb49fc5aed618c039f2c9920e
AyAyEm <andre20114meira@gmail.com>
2021-10-25 09:58:31 -0300

    Refactor: added generics instead of a fixed.. ..SkillsGroups

--- src/components/SkillSelect/index.tsx
+++ src/components/SkillSelect/index.tsx
@@ -1,27 +1,27 @@
 import { paramCase } from 'change-case';
 
-import { skillsGroups } from '#lib';
 import { SkillGroup } from '#components/SkillGroup';
 
 import './SkillSelect.scss';
 
 import type { Skill } from '../../types/skills';
 
-export interface SkillSelectProps {
-  onChange: (skill: Skill) => void,
-  skill: Skill,
+export interface SkillSelectProps<S extends Skill, K extends string> {
+  onChange: (skill: S) => void;
+  skill: S;
+  skillsGroups: Record<K, S[]>;
 }
 
-export function SkillSelect(props: SkillSelectProps) {
-  const { skill, onChange } = props;
+export function SkillSelect<S extends Skill, K extends string>(props: SkillSelectProps<S, K>) {
+  const { skill, onChange, skillsGroups } = props;
 
   return (
     <div className="skill-select">
-      {Object.entries(skillsGroups).map(([name, skills]) => (
+      {Object.entries<S[]>(skillsGroups).map(([name, skills]) => (
         <SkillGroup
           key={name}
           value={skill}
-          onChange={(_e, s: Skill) => onChange(s)}
+          onChange={(_e, s: S) => onChange(s)}
           skills={skills}
           className={paramCase(name)}
         />
