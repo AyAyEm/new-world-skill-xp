c055241ccd5a9498eaa6c0c7536634ef1496ed78
AyAyEm <andre20114meira@gmail.com>
2021-10-28 19:42:11 -0300

    Refactor: removed inRange utility

--- src/lib/utils/inRange.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Inclusive range value comparison.
- */
-export function inRange(value: number, min: number, max: number): boolean {
-  return value >= min && value <= max;
-}
-export default inRange;
